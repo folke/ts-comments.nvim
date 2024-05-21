97b7ad17f737fe7f07ac6a3c918c279dc03347e6
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-21 19:19:41 +0200

    feat: better uncommenting with whitespace fixes and multiple commmentstrings. Fixes #4

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Tiny plugin to enhance Neovim's native comments:
 
 - Easily override the comment string for a given **treesitter** language
 - Supports different comment strings for different **treesitter** node types
+- Relaxed rules for uncommenting
+- Add proper whitespace when needed
+- Supports multiple commentstrings for the same language.
+  The first one is used for adding comments.
+  All are used for uncommenting.
 
 ## ⚡️ Requirements
 
@@ -40,6 +45,17 @@ Default configuration:
     hcl = "# %s",
     html = "<!-- %s -->",
     ini = "; %s",
+    javascript = {
+      "// %s",
+      "/* %s */",
+      call_expression = "// %s",
+      jsx_attribute = "// %s",
+      jsx_element = "{/* %s */}",
+      jsx_fragment = "{/* %s */}",
+      spread_element = "// %s",
+      statement_block = "// %s",
+    },
+    lua = { "-- %s", "--- %s" }, -- langs can have multiple commentstrings
     php = "// %s",
     rego = "# %s",
     rescript = "// %s",
@@ -47,19 +63,9 @@ Default configuration:
     svelte = "<!-- %s -->",
     terraform = "# %s",
     tsx = {
-      _ = "// %s",
-      call_expression = "// %s",
-      comment = "// %s",
-      jsx_attribute = "// %s",
-      jsx_element = "{/* %s */}",
-      jsx_fragment = "{/* %s */}",
-      spread_element = "// %s",
-      statement_block = "// %s",
-    },
-    javascript = {
-      _ = "// %s",
-      call_expression = "// %s",
-      comment = "// %s",
+      "// %s", -- default commentstring when no treesitter node matches
+      "/* %s */", -- will be used for uncommenting
+      call_expression = "// %s", -- specific commentstring for call_expression
       jsx_attribute = "// %s",
       jsx_element = "{/* %s */}",
       jsx_fragment = "{/* %s */}",
--- lua/ts-comments.lua
+++ lua/ts-comments.lua
@@ -1,7 +1,11 @@
+--# selene: allow(mixed_table)
 local M = {}
 
-local defaults = {
-  ---@type table<string, string|table<string,string>>
+local get_option = vim.filetype.get_option
+
+---@class TSCommentsOptions
+---@field lang table<string, string|string[]|table<string,string>>
+local options = {
   lang = {
     astro = "<!-- %s -->",
     c = "// %s",
@@ -14,6 +18,7 @@ local defaults = {
     hcl = "# %s",
     html = "<!-- %s -->",
     ini = "; %s",
+    lua = { "-- %s", "--- %s" }, -- langs can have multiple commentstrings
     php = "// %s",
     rego = "# %s",
     rescript = "// %s",
@@ -21,9 +26,9 @@ local defaults = {
     svelte = "<!-- %s -->",
     terraform = "# %s",
     tsx = {
-      _ = "// %s",
-      call_expression = "// %s",
-      comment = "// %s",
+      "// %s", -- default commentstring when no treesitter node matches
+      "/* %s */",
+      call_expression = "// %s", -- specific commentstring for call_expression
       jsx_attribute = "// %s",
       jsx_element = "{/* %s */}",
       jsx_fragment = "{/* %s */}",
@@ -36,33 +41,94 @@ local defaults = {
     vue = "<!-- %s -->",
   },
 }
-defaults.lang.javascript = vim.deepcopy(defaults.lang.tsx)
+---@diagnostic disable-next-line: param-type-mismatch
+options.lang.javascript = vim.deepcopy(options.lang.tsx)
+
+-- Resolves the possible commentstrings for a given filetype in the current line
+---@param ft string
+---@return string[]
+local function resolve(ft)
+  local lang = vim.treesitter.language.get_lang(ft) or ft
+  local spec = options.lang[lang]
+  local ret = {} ---@type string[]
+
+  local have = {} ---@type table<string, boolean>
+  local function add(a)
+    if type(a) == "string" then
+      if not have[a] and a ~= "" then
+        have[a] = true
+        ret[#ret + 1] = a
+      end
+    elseif type(a) == "table" then
+      ---@diagnostic disable-next-line: no-unknown
+      for _, v in ipairs(a) do
+        add(v)
+      end
+      ---@diagnostic disable-next-line: no-unknown
+      for k, v in pairs(a) do
+        if type(k) == "string" then
+          add(v)
+        end
+      end
+    end
+  end
+
+  if type(spec) == "table" and not vim.islist(spec) then
+    local line = vim.fn.getline(".")
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local indent = line:match("^%s*()")
+    local ok, node = pcall(vim.treesitter.get_node, {
+      ignore_injections = false, -- include injected languages
+      -- set position to the first non whitespace character
+      pos = { vim.fn.line(".") - 1, indent and (indent - 1) or cursor[2] },
+    })
+    while ok and node do
+      if spec[node:type()] then
+        add(spec[node:type()]) -- found a commentstring for the current node
+        break
+      end
+      node = node:parent()
+    end
+  end
+
+  add(spec) -- always add all found patterns
+  add(get_option(ft, "commentstring")) -- always include the commentstring from the buffer
+  return ret
+end
+
+---@param ft string
+function M.get(ft)
+  local patterns = resolve(ft)
+  local line = vim.fn.getline(".")
+
+  local cs = nil
+  local n = math.huge
+  for _, pattern in ipairs(patterns) do -- check all patterns to check if we want to uncomment
+    local left, right = pattern:match("^%s*(.-)%s*%%s%s*(.-)%s*$") -- parse commentstring excluding whitespace
+    if left and right then
+      local l, m, r = line:match("^%s*" .. vim.pesc(left) .. "(%s*)(.-)(%s*)" .. vim.pesc(right) .. "%s*$")
+      if m and #m < n then -- most commented line
+        cs = vim.trim(left .. l .. "%s" .. r .. right) -- add correct whitespace to uncomment
+        n = #m
+      end
+      if not cs then -- first pattern is the wanted commentstring
+        cs = vim.trim(left .. " %s " .. right) -- add correct whitespace to comment
+      end
+    end
+  end
+
+  return cs
+end
 
 function M.setup(opts)
-  opts = vim.tbl_deep_extend("force", defaults, opts or {})
+  options = vim.tbl_deep_extend("force", options, opts or {})
 
-  local get_option = vim.filetype.get_option
+  ---@diagnostic disable-next-line: duplicate-set-field
   vim.filetype.get_option = function(filetype, option)
     if option ~= "commentstring" then
       return get_option(filetype, option)
     end
-    local lang = vim.treesitter.language.get_lang(filetype) or filetype
-
-    local ret = opts.lang[lang]
-    if type(ret) == "table" then
-      local ok, node = pcall(vim.treesitter.get_node, { ignore_injections = false })
-      while ok and node do
-        if ret[node:type()] then
-          ret = ret[node:type()]
-          break
-        end
-        node = node:parent()
-      end
-      if type(ret) == "table" then
-        ret = ret._
-      end
-    end
-    return ret or get_option(filetype, option)
+    return M.get(filetype)
   end
 end
 
